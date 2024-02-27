1399dd72beae5278f4f6abf67ef7ec8e12772521
Jay Pithava <js106085@gmail.com>
2024-02-27 15:58:42 +0530

    native icon added

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import {Text, View} from 'react-native';
 import React from 'react';
 
 import AllExpenses from './screens/AllExpenses';
@@ -8,15 +7,45 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import RecentExpenses from './screens/RecentExpenses';
+import {GlobalStyles} from './components/styles';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function BottomTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="RecentExpenses" component={RecentExpenses} />
-      <Tab.Screen name="AllExpenses" component={AllExpenses} />
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
+        headerTintColor: 'white',
+        tabBarStyle: {backgroundColor: GlobalStyles.colors.primary500},
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}>
+      <Tab.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({color, size}) => (
+            <AntDesign name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: 'All Expenses',
+          tabBarLabel: 'All Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="calendar" size={size} color={color} />
+          ),
+          
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -24,7 +53,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="BottomTabs" component={BottomTabs} />
+        <Stack.Screen
+          name="BottomTabs"
+          component={BottomTabs}
+          options={{headerShown: false}}
+        />
         <Stack.Screen name="ManageExpenses" component={ManageExpenses} />
       </Stack.Navigator>
     </NavigationContainer>
